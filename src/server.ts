c7c533f0c9b30142ac959b1d828c39c62c254356
raitz <raitz@rtz.localdomain>
2021-06-22 13:35:09 -0400

    Conexao com banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,9 @@
 import express from 'express';
+import "reflect-metadata";
+import './database'
+
 const app = express();
 const port = 3000;
 
-/**
- * GET    => Get information
- * POST   => Insert information
- * PUT    => Update information
- * DELETE => Delete information
- * PATCH  => Alter information
- */
-
-app.get('/get',(req, res)=>{
-  return res.send("Hello from GET");
-});
-
-app.post('/post',(req,res)=>{
-  return res.send("Hello from POST");
-});
-
-app.put('/put', (req, res)=>{
-  return res.send("Hello from PUT");
-});
-
-app.delete('/delete',(req, res)=>{
-  return res.send("Hello from DELETE");
-});
 
 app.listen(port,()=>{console.log(`Server is running at http://localhost:${port}`)});
